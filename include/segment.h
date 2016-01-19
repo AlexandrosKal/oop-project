53b53bd2c2fbed2dd0b36c334d1e004ee0ae4b0e
Mario Saldinger <mariosaldinger@gmail.com>
2016-01-19 19:22:38 +0200

    Segment: Header

--- include/segment.h
+++ include/segment.h
@@ -4,6 +4,29 @@
 namespace oop_project {
 
 class Segment {
+ public:
+  static const size_t kMaxCars;
+
+  Segment(size_t, Segment*, int, size_t, size_t);
+  ~Segment();
+
+  void Enter();
+  void Exit();
+  void Operate();
+  std::vector<Car*> Pass(size_t);
+
+  size_t num_cars() const;
+  std::vector<Car*> ready_cars() const;
+  size_t enter_junction() const;
+  void set_next(Segment* /*next*/);
+
+ private:
+  const size_t capacity_;
+  std::vector<Car*> cars_;
+  Junction* enter_junction_;
+  Segment* next_;
+  Segment* prev_;
+  const int ready_percent_;
 };
 
 }  // namespace oop_project
