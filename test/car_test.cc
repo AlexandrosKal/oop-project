ffd528ec7306cc6b467fa0d69249528bf0ac18ee
Mario Saldinger <mariosaldinger@gmail.com>
2016-01-23 00:04:46 +0200

    Car: Swapped rvalues and lvalues in asserts

--- test/car_test.cc
+++ test/car_test.cc
@@ -7,40 +7,40 @@ namespace oop_project {
 TEST(CarTest, InitializesDataMembers) {
   Segment segment(5, NULL, 5, 5, 5);
   Car car(5, &segment);
-  ASSERT_EQ(5, car.exit_junction());
-  ASSERT_EQ(false, car.ready());
-  ASSERT_EQ(&segment, car.segment());
+  ASSERT_EQ(car.exit_junction(), 5);
+  ASSERT_EQ(car.ready(), false);
+  ASSERT_EQ(car.segment(), &segment);
 }
 
 TEST(CarTest, ReturnsExitJunction) {
   Car car(5, NULL);
-  ASSERT_EQ(5, car.exit_junction());
+  ASSERT_EQ(car.exit_junction(), 5);
 }
 
 TEST(CarTest, ReturnsReady) {
   Car car(5, NULL);
-  ASSERT_EQ(false, car.ready());
+  ASSERT_EQ(car.ready(), false);
 }
 
 TEST(CarTest, ReturnsSegment) {
   Segment segment(5, NULL, 5, 5, 5);
   Car car(5, &segment);
-  ASSERT_EQ(&segment, car.segment());
+  ASSERT_EQ(car.segment(), &segment);
 }
 
 TEST(CarTest, ChangesReadyValue) {
   Car car(5, NULL);
   car.set_ready(true);
-  ASSERT_EQ(true, car.ready());
+  ASSERT_EQ(car.ready(), true);
   car.set_ready(false);
-  ASSERT_EQ(false, car.ready());
+  ASSERT_EQ(car.ready(), false);
 }
 
 TEST(CarTest, ChangesSegmentValue) {
   Segment segment(5, NULL, 5, 5, 5);
   Car car(5, NULL);
   car.set_segment(&segment);
-  ASSERT_EQ(&segment, car.segment());
+  ASSERT_EQ(car.segment(), &segment);
 }
 
 }  // namespace oop_project
