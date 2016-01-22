9940ee3f32ca95c37e985ddc8a5f2ea32efb05c8
Mario Saldinger <mariosaldinger@gmail.com>
2016-01-22 20:43:59 +0200

    Segment: Added tests

--- test/segment_test.cc
+++ test/segment_test.cc
@@ -1,58 +1,95 @@
 #include "segment.h"
+#include "gtest/gtest.h"
 
 namespace oop_project {
 
-TEST(SegmentTest, EnterInsertsReadyCars) {
-  Segment prev_segment = new Segment(10, NULL, 50, 5);
-  Segment segment(10, prev_segment, 50, 5);
-  size_t prev_ready = (prev_segment.ready_cars()).size();
-  size_t num_before_enter = segment.num_cars();
-  segment.Enter();
-  ASSERT_LE(segment.num_cars(), 10)
-  ASSERT_LE(segment.num_cars() , num_before_enter + prev_ready)
+TEST(SegmentTest, CreatesRandomCars) {
+  Segment* segment = new Segment(10, NULL, 50, 5, 5);
+  ASSERT_LE(segment->num_cars(), 10);
+
+  ASSERT_LE(segment->ready_cars().size(), 50 * segment->num_cars() / 100);
+  delete segment;
+
+  segment = new Segment(2, NULL, 50, 5, 5);
+  ASSERT_LE(segment->num_cars(), 2);
+  delete segment;
 }
 
-TEST(SegmentTest, RemovesCarsFromSegment) {
-  Segment segment(10, NULL, 50, 5);
-  std::vector<Cars*> ready_cars = segment.ready_cars();
-  size_t ready_to_exit = 0;
+TEST(SegmentTest, EntersLessThanCapacity) {
+  Segment prev_segment(10, NULL, 50, 5, 5);
+  Segment segment(10, &prev_segment, 50, 5, 5);
+
+  size_t prev_ready = prev_segment.ready_cars().size();
+  size_t num_before_enter = segment.num_cars();
+  segment.Enter();
+  ASSERT_LE(segment.num_cars(), 10);
+  ASSERT_LE(segment.num_cars(), num_before_enter + prev_ready);
+}
+
+TEST(SegmentTest, RemovesCarsFromFreeway) {
+  Segment segment(10, NULL, 50, 5, 5);
+  std::vector<Car*> ready_cars = segment.ready_cars();
   size_t num_before_exit = segment.num_cars();
-  for(size_t i = 0; i < ready_cars.size(); ++i) {
-    if(ready_cars[i]->exit_junction() == segment->enter_junction()() + 1) {
-      ready_to_exit++;
+
+  size_t ready_to_exit = 0;
+  for (size_t i = 0; i < ready_cars.size(); ++i) {
+    if (ready_cars[i]->exit_junction() == segment.enter_junction() + 1) {
+      ++ready_to_exit;
     }
   }
+
   segment.Exit();
-  ASSERT_LE(ready_to_exit, num_before_exit);
   ASSERT_EQ(segment.num_cars(), num_before_exit - ready_to_exit);
 }
 
-TEST(SegmentTest, PassesReadyCarsToNext) {
-  Segment segment(10, NULL, 50, 5);
-  Segment next_segment = new Segment(10, NULL, 50, 5);
-  std::vector<Cars*> ready_cars = segment.ready_cars();
+TEST(SegmentTest, Operates) {
+  Segment segment(10, NULL, 50, 5, 5);
+  std::vector<Car*> prev_ready_cars = segment.ready_cars();
+  segment.Operate();
+  std::vector<Car*> ready_cars = segment.ready_cars();
+
+  ASSERT_GE(ready_cars.size(), prev_ready_cars.size());
+  ASSERT_LE(ready_cars.size() - prev_ready_cars.size(),
+            50 * segment.num_cars() / 100);
+}
+
+TEST(SegmentTest, PassesReadyCars) {
+  Segment segment(10, NULL, 50, 5, 5);
+  Segment next_segment(10, NULL, 50, 5, 5);
+  segment.set_next(&next_segment);
+
+  std::vector<Car*> ready_cars = segment.ready_cars();
   size_t ready_to_pass = 0;
-  for(size_t i = 0; i < ready_cars.size(); ++i) {
-    if(ready_cars[i]->exit_junction() != segment->enter_junction() + 1) {
-        ready_to_pass++;
+  for (size_t i = 0; i < ready_cars.size(); ++i) {
+    if (ready_cars[i]->exit_junction() != segment.enter_junction() + 1) {
+        ++ready_to_pass;
     }
   }
-  size_t number_before_pass = next_segment->num_cars();
-  segment.Pass()
-  ASSERT_LE(next_segment->num_cars(), 10);
-  ASSERT_EQ(segment.num_cars(), number_before_pass - ready_to_pass);
+
+  size_t num_before_pass = segment.num_cars();
+  segment.Pass(5);
+  ASSERT_GE(segment.num_cars(), num_before_pass - 5);
+  ASSERT_GE(segment.num_cars(), num_before_pass - ready_to_pass);
+  ASSERT_LE(next_segment.num_cars(), 10);
 }
 
-TEST(SegmentTest, SetsReadyPercent) {
-  Segment segment(10, NULL, 50, 5);
-  size_t num_ready = (segment.ready_cars()).size()
-  size_t not_ready_before_operate = segment.num_cars() - num_ready;
-}
-/*TEST(SegmentTest, SetsNextSegment) {
- Segment segment(10, NULL, 50, 5);
- Segment next_segment = new Segment(10, NULL, 50, 5);
- segment.set_next(next_segment);
+TEST(SegmentTest, ReturnsReadyCars) {
+  Segment segment(10, NULL, 50, 5, 5);
+  std::vector<Car*> ready_cars = segment.ready_cars();
 
-} may not be needed*/
+  ASSERT_LE(ready_cars.size(), segment.num_cars());
+  for (size_t i = 0; i < ready_cars.size(); ++i) {
+    ASSERT_EQ(ready_cars[i]->ready(), true);
+  }
+}
+
+TEST(SegmentTest, ReturnsEnterJunction) {
+  Segment segment0(10, NULL, 50, 5, 5);
+  Segment segment1(10, NULL, 50, 5, 5);
+  Segment segment2(10, NULL, 50, 5, 5);
+  ASSERT_GE(segment0.enter_junction(), 1);
+  ASSERT_EQ(segment1.enter_junction(), segment0.enter_junction() + 1);
+  ASSERT_EQ(segment2.enter_junction(), segment0.enter_junction() + 2);
+}
 
 }  // namespace oop_project
