f520a9d7830f4396aec5bfdae67ac1600de1efa0
Mario Saldinger <mariosaldinger@gmail.com>
2016-02-20 23:07:02 +0200

    Car: Removed unnecessary tests

--- test/car_test.cc
+++ test/car_test.cc
@@ -12,22 +12,6 @@ TEST(CarTest, InitializesDataMembers) {
   ASSERT_EQ(car.segment(), &segment);
 }
 
-TEST(CarTest, ReturnsExitJunction) {
-  Car car(5, NULL);
-  ASSERT_EQ(car.exit(), 5);
-}
-
-TEST(CarTest, ReturnsReady) {
-  Car car(5, NULL);
-  ASSERT_EQ(car.ready(), false);
-}
-
-TEST(CarTest, ReturnsSegment) {
-  Segment segment(5, NULL, 5, 5, 5);
-  Car car(5, &segment);
-  ASSERT_EQ(car.segment(), &segment);
-}
-
 TEST(CarTest, ChangesReadyValue) {
   Car car(5, NULL);
   car.set_ready(true);
