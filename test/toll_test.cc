0c27762ecea3377f232835a2b515cc094c8b31ea
Mario Saldinger <mariosaldinger@gmail.com>
2016-02-20 23:08:09 +0200

    Toll: Added tests

--- test/toll_test.cc
+++ test/toll_test.cc
@@ -3,10 +3,17 @@
 
 namespace project {
 
-TEST(TollTest, CreatesCar) {
+TEST(TollTest, CreatesCars) {
   Toll toll(5, 10);
   ASSERT_GE(toll.num_cars(), 1);
   ASSERT_LE(toll.num_cars(), Toll::kMaxCars);
+
+  const std::vector<Car*>& cars = toll.cars();
+  for (size_t i = 0; i < cars.size(); ++i) {
+    ASSERT_GT(cars[i]->exit(), 5);
+    ASSERT_LT(cars[i]->exit(), 10);
+    ASSERT_EQ(NULL, cars[i]->segment());
+  }
 }
 
 TEST(TollTest, AddsCar) {
@@ -16,6 +23,7 @@ TEST(TollTest, AddsCar) {
   Car* car = new Car(10, NULL);
   toll.Add(car);
   ASSERT_EQ(toll.num_cars(), prev_num_cars + 1);
+  ASSERT_EQ(toll.cars()[prev_num_cars], car);
 }
 
 TEST(TollTest, HandlesZeroRemoval) {
@@ -30,6 +38,7 @@ TEST(TollTest, RemovesSomeCars) {
 
   std::vector<Car*> cars = toll.Remove(prev_num_cars - 1);
   ASSERT_EQ(cars.size(), prev_num_cars - 1);
+  ASSERT_EQ(toll.num_cars(), prev_num_cars - cars.size());
   for (size_t i = 0; i < cars.size(); ++i) {
     delete cars[i];
   }
@@ -41,6 +50,7 @@ TEST(TollTest, HandlesOverflowRemoval) {
 
   std::vector<Car*> cars = toll.Remove(num_cars + 10);
   ASSERT_LE(cars.size(), num_cars);
+  ASSERT_EQ(toll.num_cars(), 0);
   for (size_t i = 0; i < cars.size(); ++i) {
     delete cars[i];
   }
@@ -55,14 +65,6 @@ TEST(TollTest, RemovesAllCars) {
   ASSERT_EQ(toll.num_cars(), 0);
   for (size_t i = 0; i < cars.size(); ++i) {
     delete cars[i];
-  }
-}
-
-TEST(TollTest, CreatesCarsWithGreaterExitJunction) {
-  Toll toll(5, 10);
-  std::vector<Car*> cars = toll.cars();
-  for (size_t i = 0; i < cars.size(); ++i) {
-    ASSERT_GT(cars[i]->exit(), 5);
   }
 }
 
